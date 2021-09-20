a4697f0c38a0c26e2bb0785c696c54a9fe822d70
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2021-09-20 13:37:47 +0530

    Initialize Structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = props => {
+	return (
+		<button className="btn">
+		button
+		</button>
+	);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Display = props => {
+	return <h1>0</h1>;
+}
+
+const KeyPad = props => {
+	return <h1>Keys</h1>;
+}
+
+const Calculator = props => {
+	return (
+		<React.Fragment>
+			<Display />
+			<KeyPad />
+		</React.Fragment>
+	);
+}
+
+const App = props => {
+	return (
+		<React.Fragment>
+			<h1>Calculator</h1>
+			<Calculator />
+		</React.Fragment>
+	);
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
