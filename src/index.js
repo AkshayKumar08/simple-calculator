e1685ed952454ca28b94a5b863d7dbcc4426e39b
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2021-09-22 15:21:06 +0530

    Making Interactive Component

--- src/index.js
+++ src/index.js
@@ -1,75 +1,106 @@
-import React, {Fragment} from 'react'
+import { Fragment, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const Button = props => {
-	return (
-		<button className={`${props.class? props.class: "btn"}`}>
-		{props.value}
+const Button = ({ styleName, value, setInput }) => {
+
+    const isDigit = (x) => {
+        return '0' <= x && x <= '9';
+    }
+
+    const onButtonClick = (value) => {
+        if (value === 'C') {
+            setInput('0')
+        } else if (value === '⌫') {
+            setInput(input => input.slice(0, input.length - 1))
+        } else if (value === '=') {
+        	// TODO
+        } else {
+            setInput(input => {
+                if (input.charAt(0) === '0' &&
+                    input.length == 1 &&
+                    (isDigit(value) || value === '-')) {
+                    return value;
+                } else if (isDigit(value) ||
+                    isDigit(input.charAt(input.length - 1))) {
+                    return input + value;
+                } else {
+                    return input.slice(0, input.length - 1) + value;
+                }
+            });
+        }
+    }
+
+    return (
+        <button 
+			className={`btn ${styleName}`}
+			onClick={() => onButtonClick(value)}
+		>
+		{value}
 		</button>
-	);
+    );
 }
 
-const Display = props => {
-	return (
-		<React.Fragment>
+const Display = ({ styleName, value }) => {
+    return (
+        <Fragment>
 			<input
 				type="text"
-				className={props.class}
-				value={props.value}
-				readonly=""/>
-		</React.Fragment>
-	);
+				className={`display ${styleName}`}
+				value={value}
+				readOnly/>
+		</Fragment>
+    );
 }
 
-const KeyPad = props => {
-	return (
-		<div className="keypad">
-			<Button class="btn symbol" value="C"/>
-			<Button class="btn symbol" value={<Fragment>&divide;</Fragment>}/>
-			<Button class="btn symbol" value={<Fragment>&times;</Fragment>}/>
-			<Button class="btn symbol" value="⌫"/>
+const KeyPad = ({ setInput }) => {
+
+    return (
+        <div className="keypad" >
+			<Button styleName="symbol" value="C" setInput={setInput}/>
+			<Button styleName="symbol" value="÷" setInput={setInput}/>
+			<Button styleName="symbol" value="×" setInput={setInput}/>
+			<Button styleName="symbol" value="⌫" setInput={setInput}/>
 			
-			<Button value="7"/>
-			<Button value="8"/>
-			<Button value="9"/>
-			<Button class="btn symbol" value="-"/>
+			<Button styleName="number" value="7" setInput={setInput}/>
+			<Button styleName="number" value="8" setInput={setInput}/>
+			<Button styleName="number" value="9" setInput={setInput}/>
+			<Button styleName="symbol" value="-" setInput={setInput}/>
 			
-			<Button value="4"/>
-			<Button value="5"/>
-			<Button value="6"/>
-			<Button class="btn symbol" value="+"/>
+			<Button styleName="number" value="4" setInput={setInput}/>
+			<Button styleName="number" value="5" setInput={setInput}/>
+			<Button styleName="number" value="6" setInput={setInput}/>
+			<Button styleName="symbol" value="+" setInput={setInput}/>
 			
-			<Button value="1"/>
-			<Button value="2"/>
-			<Button value="3"/>
-			<Button class="btn equals" value="="/>
+			<Button styleName="number" value="1" setInput={setInput}/>
+			<Button styleName="number" value="2" setInput={setInput}/>
+			<Button styleName="number" value="3" setInput={setInput}/>
+			<Button styleName="symbol equals" value="=" setInput={setInput}/>
 			
-			<Button class="btn symbol" value="%"/>
-			<Button value="0"/>
-			<Button value="."/>
-			
+			<Button styleName="number zero" value="0" setInput={setInput}/>
 		</div>
-	);
+    );
 }
 
 const Calculator = props => {
-	return (
-		<div className="calculator">
-			<Display class="display input" value="0"/>
-			<Display class="display output" value="0"/>
-			<KeyPad />
+    const [input, setInput] = useState("0");
+    const [output, setOutput] = useState("");
+    return (
+        <div className="calculator">
+			<Display styleName="input" value={input}/>
+			<Display styleName="output" value={output}/>
+			<KeyPad setInput={input => setInput(input)}/>
 		</div>
-	);
+    );
 }
 
 const App = props => {
-	return (
-		<React.Fragment>
+    return (
+        <Fragment>
 			<h1>Calculator</h1>
 			<Calculator/>
-		</React.Fragment>
-	);
+		</Fragment>
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
