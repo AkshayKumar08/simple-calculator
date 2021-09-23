3c2cbe60825ab84b714496083fb0ac2de1fd6932
R M AKSHAY KUMAR <rmakshaykumar08@gmail.com>
2021-09-23 18:32:20 +0530

    Calculator Complete

--- src/index.js
+++ src/index.js
@@ -2,39 +2,16 @@ import { Fragment, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const Button = ({ styleName, value, setInput }) => {
-
-    const isDigit = (x) => {
-        return '0' <= x && x <= '9';
-    }
-
-    const onButtonClick = (value) => {
-        if (value === 'C') {
-            setInput('0')
-        } else if (value === '⌫') {
-            setInput(input => input.slice(0, input.length - 1))
-        } else if (value === '=') {
-        	// TODO
-        } else {
-            setInput(input => {
-                if (input.charAt(0) === '0' &&
-                    input.length == 1 &&
-                    (isDigit(value) || value === '-')) {
-                    return value;
-                } else if (isDigit(value) ||
-                    isDigit(input.charAt(input.length - 1))) {
-                    return input + value;
-                } else {
-                    return input.slice(0, input.length - 1) + value;
-                }
-            });
-        }
+const Button = ({ styleName, value, onClick }) => {
+    const onButtonClick = (event) => {
+        onClick(event.target.value);
     }
 
     return (
         <button 
 			className={`btn ${styleName}`}
-			onClick={() => onButtonClick(value)}
+			onClick={onButtonClick}
+			value={value}
 		>
 		{value}
 		</button>
@@ -53,31 +30,31 @@ const Display = ({ styleName, value }) => {
     );
 }
 
-const KeyPad = ({ setInput }) => {
+const KeyPad = ({ onClick }) => {
 
     return (
         <div className="keypad" >
-			<Button styleName="symbol" value="C" setInput={setInput}/>
-			<Button styleName="symbol" value="÷" setInput={setInput}/>
-			<Button styleName="symbol" value="×" setInput={setInput}/>
-			<Button styleName="symbol" value="⌫" setInput={setInput}/>
+			<Button styleName="symbol" value="C" onClick={onClick}/>
+			<Button styleName="symbol" value="÷" onClick={onClick}/>
+			<Button styleName="symbol" value="×" onClick={onClick}/>
+			<Button styleName="symbol" value="⌫" onClick={onClick}/>
 			
-			<Button styleName="number" value="7" setInput={setInput}/>
-			<Button styleName="number" value="8" setInput={setInput}/>
-			<Button styleName="number" value="9" setInput={setInput}/>
-			<Button styleName="symbol" value="-" setInput={setInput}/>
+			<Button styleName="number" value="7" onClick={onClick}/>
+			<Button styleName="number" value="8" onClick={onClick}/>
+			<Button styleName="number" value="9" onClick={onClick}/>
+			<Button styleName="symbol" value="-" onClick={onClick}/>
 			
-			<Button styleName="number" value="4" setInput={setInput}/>
-			<Button styleName="number" value="5" setInput={setInput}/>
-			<Button styleName="number" value="6" setInput={setInput}/>
-			<Button styleName="symbol" value="+" setInput={setInput}/>
+			<Button styleName="number" value="4" onClick={onClick}/>
+			<Button styleName="number" value="5" onClick={onClick}/>
+			<Button styleName="number" value="6" onClick={onClick}/>
+			<Button styleName="symbol" value="+" onClick={onClick}/>
 			
-			<Button styleName="number" value="1" setInput={setInput}/>
-			<Button styleName="number" value="2" setInput={setInput}/>
-			<Button styleName="number" value="3" setInput={setInput}/>
-			<Button styleName="symbol equals" value="=" setInput={setInput}/>
+			<Button styleName="number" value="1" onClick={onClick}/>
+			<Button styleName="number" value="2" onClick={onClick}/>
+			<Button styleName="number" value="3" onClick={onClick}/>
+			<Button styleName="symbol equals" value="=" onClick={onClick}/>
 			
-			<Button styleName="number zero" value="0" setInput={setInput}/>
+			<Button styleName="number zero" value="0" onClick={onClick}/>
 		</div>
     );
 }
@@ -85,11 +62,71 @@ const KeyPad = ({ setInput }) => {
 const Calculator = props => {
     const [input, setInput] = useState("0");
     const [output, setOutput] = useState("");
+
+    const isDigit = (x) => {
+        return '0' <= x && x <= '9';
+    }
+
+    const evaluate = (expression) => {
+        try {
+            expression = expression.replace(/÷/g, '/').replace(/×/g, '*')
+            return eval(expression).toString();
+        } catch (err) { return "Invalid"; }
+    }
+
+    const handleClick = (value) => {
+        if (input === 'Infinity' || input === 'NaN' || input === 'error') {
+            setOutput("");
+            setInput("0");
+        }
+        if (value === 'C') {
+            setOutput("");
+            setInput("0");
+        } else if (value === '⌫') {
+            setInput(prevInput => (prevInput.length <= 1) ? "0" : prevInput.slice(0, input.length - 1));
+        } else if (value === '=') {
+            try {
+                setInput(prevInput => {
+                    const result = evaluate(prevInput)
+                    if (result !== "Invalid") {
+                        return result;
+                    } else return "error";
+                });
+                setOutput("");
+            } catch (err) {
+                setOutput('error');
+            }
+            return;
+        } else {
+            setInput(prevInput => {
+                if (prevInput.charAt(0) === '0' && // if only '0' is present in input state
+                    prevInput.length === 1 &&
+                    (isDigit(value) || value === '-')) {
+                    return value;
+                } else if (isDigit(value) ||
+                    isDigit(prevInput.charAt(prevInput.length - 1))) {
+                    // if last input is a number or next value is a number
+                    return prevInput + value;
+                } else {
+                    // if operator is present input and next value is also operator
+                    return prevInput.slice(0, prevInput.length - 1) + value;
+                }
+            });
+        }
+        try {
+            setOutput(prevOutput => {
+                const result = evaluate(input);
+                if (result === "Invalid") return prevOutput;
+                return result;
+            });
+        } catch (err) {}
+    }
+
     return (
         <div className="calculator">
 			<Display styleName="input" value={input}/>
 			<Display styleName="output" value={output}/>
-			<KeyPad setInput={input => setInput(input)}/>
+			<KeyPad onClick={(e) => handleClick(e)}/>
 		</div>
     );
 }
